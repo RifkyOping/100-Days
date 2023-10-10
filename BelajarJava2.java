bd4d325843427d029be5b8d2ba8ba938de4df955
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-10 10:49:50 +0800

    Hari kedua
    Input

---
+++ BelajarJava2.java
@@ -0,0 +1,35 @@
+package baru;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        Scanner input = new Scanner(System.in);
+     String nama,nim, prodi;
+     char kelas;
+     int umur;
+     float bb;
+     
+     System.out.print("Nama : ");
+     nama = input.nextLine();
+        
+     System.out.print("NIM : ");
+     nim = input.nextLine();
+        
+     System.out.print("Prodi : ");
+     prodi = input.next();
+     
+     System.out.print("Kelas : ");
+     kelas = input.next().charAt(0);
+     
+     System.out.print("Umur : ");
+     umur = input.nextInt();
+     
+     System.out.print("Berat Badan : ");
+     bb = input.nextFloat();
+        
+        
+    }
+    
+}
