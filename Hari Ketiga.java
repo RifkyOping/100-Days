497350485a14bf9d68064072418e9f7d1cefa847
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-11 11:45:52 +0800

    Hari Ketiga

---
+++ Hari Ketiga.java
@@ -0,0 +1,29 @@
+package Aritmatika;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int a = 6, b = 4, c;
+        
+        c = a + b;
+        System.out.println(c);
+        
+        c = a - b;
+        System.out.println(c);
+        
+        c = a * b;
+        System.out.println(c);
+        
+        c = a / b;
+        System.out.println(c);
+        
+        c = a % b;
+        System.out.println(c);
+        
+        
+        
+        
+    }
+    
+}
