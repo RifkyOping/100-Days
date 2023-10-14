48247cc8dbb7681aa07659f4509556b0b43159f5
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-14 15:35:37 +0800

    Hari Keenam

---
+++ Hari Keenam.java
@@ -0,0 +1,14 @@
+package Penugasan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int nilaiA = 5, nilaiB = 10;
+        
+        nilaiA *= nilaiB;
+        
+        System.out.println(nilaiA);
+    }
+    
+}
