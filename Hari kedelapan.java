5eb3a069e3b3534e1d4acb100c6126541b5355a9
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-16 09:14:56 +0800

    Hari kedelapan

---
+++ Hari kedelapan.java
@@ -0,0 +1,14 @@
+package Penugasan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int nilaiA = 20, nilaiB = 6;
+        
+        nilaiA %= nilaiB;
+        
+        System.out.println(nilaiA);
+    }
+    
+}
