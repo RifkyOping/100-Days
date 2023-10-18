19134388824ae711250fd44fdc9e6e52f56bcfc0
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-18 17:53:36 +0800

    Increment dan Decrement

---
+++ Days_10.java
@@ -0,0 +1,30 @@
+package Increment_10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a, b, c, d;
+        
+        // Increment Post => (a = a + 1)
+        a = 1;
+        a++; // ++ artinya tambah 1
+        System.out.println(a);
+        
+        // Increment pree => (b = 1 + b)
+        b = 2;
+        ++b; 
+        System.out.println(b);
+        
+        // Decrement Post => (c = c - 1)
+        c = 3;
+        c--; // -- artinya kurang 1
+        System.out.println(c);
+        
+        // Decrement Pree => (d = 1 - d)
+        d = 4;
+        --d;
+        System.out.println(d);
+        
+    }
+    
+}
