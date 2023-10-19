adbfa9003bc1f2a85d18b275f4b7c0b2f7366027
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-19 11:35:19 +0800

    Operator Pembanding

---
+++ Day_11.java
@@ -0,0 +1,35 @@
+package pembanding;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 4, b = 5;
+        boolean hasil;
+        
+        // Apakah a lebih kecil dari b ?
+        hasil = a < b;
+        System.out.println(hasil);
+        
+        // Apakah a lebih besar dari b ?
+        hasil = a > b;
+        System.out.println(hasil);
+        
+        // Apakah a lebih kecil atau sama dengan b ?
+        hasil = a <= b;
+        System.out.println(hasil);
+        
+        
+        // Apakah a lebih besar atau sama dengan b ?
+        hasil = a >= b;
+        System.out.println(hasil);
+        
+        // Apakah a sama dengan b ?
+        hasil = a == b;
+        System.out.println(hasil);
+        
+        // Apakah a tidak sama dengan b ?
+        hasil = a != b;
+        System.out.println(hasil);
+    }
+    
+}
