dea20350da07f67fd25bd0e18acc5ce31c7f7f9d
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-24 09:38:53 +0800

    Percabangan Switch-Case

---
+++ Day_016.java
@@ -0,0 +1,60 @@
+package switchJava;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        /*
+        Switch-Case merupakan bentuk lain dari percabangan if-else.
+        Namun yang membedakan adalah formatnya.
+        Berbeda dari if-else yang berisi perbandingan, percabangan switch-case
+        hanya berisi hasil.
+        */
+        int angka = 1;
+        
+        switch(angka){
+            case 1:
+            System.out.println("Good");
+            break;
+            
+            case 2:
+            System.out.println("Nice");
+            break;
+            
+            default:
+            System.out.println("Lawak");
+        }
+            
+       int angka2 = 2;
+        
+        switch(angka2){
+            case 1:
+            System.out.println("Good");
+            break;
+            
+            case 2:
+            System.out.println("Nice");
+            break;
+            
+            default:
+            System.out.println("Lawak");
+        }
+        
+        int angka3 = 3;
+        
+        switch(angka){
+            case 1:
+            System.out.println("Good");
+            break;
+            
+            case 2:
+            System.out.println("Nice");
+            break;
+            
+            default:
+            System.out.println("Lawak");
+        }    
+           
+    }
+    
+}
