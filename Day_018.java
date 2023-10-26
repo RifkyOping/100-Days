4bb755dc9f1eb2f56c806d938075b15c2d8b9902
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-26 21:38:39 +0800

    Looping For

---
+++ Day_018.java
@@ -0,0 +1,17 @@
+package Loop;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /*
+        Perulangan For : Merupakan perulangan counted loop, yakni
+                         perulangan yang tentu dan terhitung atau memiliki
+                         jumlah yang pasti.
+        */
+        for(int a = 0; a <= 50; a++){
+            System.out.print(a+" ");
+        }
+        
+    }
+    
+}
