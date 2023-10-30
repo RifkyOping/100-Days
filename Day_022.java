d8708685e20e7e5aa0476251005c39db35fdb16c
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-10-30 20:31:31 +0800

    Array

---
+++ Day_022.java
@@ -0,0 +1,17 @@
+package Data_Array;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /*
+        Array adalah sebuah variabel yang bisa menyimpan banyak data dalam satu variabel.
+                     Array menggunakan indeks untuk memudahkan akses terhadap data yang disimpannya.
+                     Indeks Array selalu dumilai dari 0.
+        */
+        //Membuat Array
+        String [] prenn = {"adi", "aco", "karunia"};
+        //Pengambilan Data Array
+        System.out.println(prenn[0]);
+        System.out.println(prenn[2]);
+    }
+}
