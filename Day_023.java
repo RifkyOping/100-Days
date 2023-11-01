b2f3fedee2a850206872c6be513624957d3f6541
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-11-01 13:59:05 +0800

    Array multidimensi

--- Day_023.java
+++ Day_023.java
@@ -10,17 +10,35 @@ public class Main {
         */
         //Membuat Array
         String [][] prenn = {
-            {"Brody","081315267815"},
-            {"Lukas","082425671098"},
-            {"Oskar","083729238387"},
-            {"Parman","08491827722"},
-            {"Kaco'","085326749177"}
+            {"Brody","081315267815","Hindu"},
+            {"Lukas","082425671098","Budha"},
+            {"Oskar","083729238387","Kristen"},
+            {"Parman","08491827722","Islam"},
+            {"Kaco'","085326749177","Islam"}
         };
         //Pengambilan Data Array
-        for(int a = 0; a < prenn.length; a++){
-        System.out.println("Nama : "+prenn[a][0]);
-        System.out.println("Nomor : "+prenn[a][1]);
-            System.out.println("");
-        }
+        System.out.println("Nama  : "+prenn[0][0]);
+        System.out.println("Nomor : "+prenn[0][1]);
+        System.out.println("Agama : "+prenn[0][2]);
+        System.out.println("");
+        
+        System.out.println("Namab : "+prenn[1][0]);
+        System.out.println("Nomor : "+prenn[1][1]);
+        System.out.println("Agama : "+prenn[1][2]);
+        System.out.println("");
+        
+        System.out.println("Nama  : "+prenn[2][0]);
+        System.out.println("Nomor : "+prenn[2][1]);
+        System.out.println("Agama : "+prenn[2][2]);
+        System.out.println("");
+        
+        System.out.println("Nama  : "+prenn[3][0]);
+        System.out.println("Nomor : "+prenn[3][1]);
+        System.out.println("Agama : "+prenn[3][2]);
+        System.out.println("");
+        
+        System.out.println("Nama  : "+prenn[4][0]);
+        System.out.println("Nomor : "+prenn[4][1]);
+        System.out.println("Agama : "+prenn[4][2]);
     }
 }
