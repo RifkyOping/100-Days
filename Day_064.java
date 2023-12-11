401ec4a55607a7e9a601fdaa0d1fac12f9fe5dec
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2023-12-11 20:14:53 +0800

    Mencari Nilai terkecil Array 2 Dimensi

---
+++ Day_064.java
@@ -0,0 +1,36 @@
+package D64_;
+
+import java.util.Scanner;
+
+public class D64_ {
+    static void Terkecil(int arr[][]){
+        int hasil = arr[0][0];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(arr[i][j] < hasil){
+                    hasil = arr[i][j];
+                }
+            }
+        }
+        System.out.println("Nilai Terkecilnya Adalah "+hasil);
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Baris : ");
+        int baris = input.nextInt();
+        System.out.print("Masukkan Kolom : ");
+        int kolom = input.nextInt();
+        int arr [][] = new int [baris][kolom];
+        
+        System.out.println();
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print("Masukkan Data : ");
+                arr[i][j] = input.nextInt();
+            }
+        }
+        System.out.println();
+        Terkecil(arr);
+    }
+}
