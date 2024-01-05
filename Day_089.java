760e3c69733ab71a872d7c79d8863b292b797a02
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2024-01-05 19:35:29 +0800

    Membuat Tabel Penjumlahan

---
+++ Day_089.java
@@ -0,0 +1,18 @@
+package D89;
+import java.util.Scanner;
+public class D89 {
+    
+    public static void main(String[] args) {
+        System.out.println("Tabel Penjumlahan");
+        while(true){
+            Scanner in = new Scanner(System.in);
+            System.out.print("Masukkan Angka Penjumlahan : ");
+            int angka = in.nextInt();
+            
+            for(int i = 1; i <= 10; i++){
+                System.out.println(i+" x "+angka+" = "+(angka+i));
+            }
+            System.out.println();
+        }
+    }
+}
