10223ae48d88f9ffaf58d93fc94694a9f058ff70
RifkyOping <147174677+RifkyOping@users.noreply.github.com>
2024-01-05 21:06:32 +0800

    Membuat Tabel Penjumlahan

--- Day_089.java
+++ Day_089.java
@@ -10,7 +10,7 @@ public class D89 {
             int angka = in.nextInt();
             
             for(int i = 1; i <= 10; i++){
-                System.out.println(i+" x "+angka+" = "+(angka+i));
+                System.out.println(i+" + "+angka+" = "+(angka+i));
             }
             System.out.println();
         }
